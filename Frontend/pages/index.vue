35c55b12052db8968b1626026a8826025f6c4d09
Enzo Gulppi <enzo.gulppi@usach.cl>
2023-10-12 22:01:38 -0300

    Comienzo Front

---
+++ Frontend/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    id="index"
+    class="d-flex flex-column justify-content-center align-items-center vh-100"
+  >
+    <h1 class="text-center">Plataforma de emergencia para Voluntariados</h1>
+    <div class="mt-4">
+      <a href="/login" class="btn btn-primary">Iniciar sesión</a>
+      <a href="/register" class="btn btn-success">Registrarte</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
