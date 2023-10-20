6683aa8195f1244b0111732234ae313ecbd3aa0b
Marcelo <marcelo.godoy@usach.cl>
2023-10-20 18:40:43 -0300

    Cambios

---
+++ src/main/java/cl/tbd/ejemplo1/services/VoluntarioService.java
@@ -0,0 +1,7 @@
+package cl.tbd.ejemplo1.services;
+
+public class VoluntarioService {
+    //Crear formulario de registro de voluntario en el sistema (ingreso de datos de perfil)
+    //Crear formulario de registro de voluntario en una emergencia (ingreso de datos de perfil)
+
+}
