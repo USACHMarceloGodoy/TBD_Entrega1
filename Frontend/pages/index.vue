fc8c58d9ffab6719c6d440a6bd14d214928241e4
Enzo Gulppi <enzo.gulppi@usach.cl>
2023-10-21 14:57:49 -0300

    Cambio en la pagina inicial

--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -5,8 +5,8 @@
   >
     <h1 class="text-center">Plataforma de Emergencia para Voluntariados</h1>
     <div class="mt-4">
-      <a href="/login" class="btn btn-primary">Iniciar sesión</a>
-      <a href="/register" class="btn btn-success">Registrarte</a>
+      <a href="/login" class="btn btn-success">Iniciar sesión</a>
+      <a href="/register" class="btn btn-secondary">Registrarte</a>
     </div>
   </div>
 </template>
@@ -14,3 +14,18 @@
 <script>
 export default {}
 </script>
+<style scooped>
+#index {
+  color: white;
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    0deg,
+    rgb(92, 64, 64) 0%,
+    rgb(95, 61, 61) 5%,
+    rgb(104, 59, 59) 10%,
+    rgb(97, 58, 58) 17%,
+    rgb(114, 53, 53) 24%,
+    rgb(255, 0, 0) 100%
+  );
+}
+</style>
