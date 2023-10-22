e1257637c2f4b91ab57e0d006a39dca1a7d673ad
Alexandra Navarro <alexandra.navarro@usach.cl>
2023-10-21 22:21:55 -0300

    modificacion perfil (diseño)

--- Frontend/pages/perfil.vue
+++ Frontend/pages/perfil.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
+  <div class='perfil-page'>
     <NavBar/>
-    <h1 style='text-align: center'>Perfil de voluntario</h1>
     <br>
-    <table class="table table-striped d-flex justify-content-center">
+    <h1 class='titulo' style='text-align: center'>Perfil de voluntario</h1>
+    <br>
+    <table class="table table-striped d-flex  table-container">
       <tbody>
         <tr>
           <th>ID</th>
@@ -58,6 +59,22 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+  .perfil-page{
+    background-image: url('/fondo.jpg');
+    background-size: cover; /* Ajusta el tamaño de la imagen para cubrir el contenedor */
+    background-repeat: no-repeat; /* Evita que la imagen se repita */
+    background-position: center; /* Centra la imagen en el contenedor */
+    min-height: 100vh;
+
+    .table-container {
+      float: left;
+      padding-left: 200px; /* Agrega un espaciado alrededor de la tabla */
+    }
+
+  .titulo{
+    background: #ffffff;
+  }
+}
 
 </style>
